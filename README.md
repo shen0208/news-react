907127fc24acd3e526f0aab402c65754c76ebdb7
shen0208 <843308508@qq.com>
2017-06-15 20:15:22 +0800

    add app

--- README.md
+++
@@ -1,2 +0,0 @@
-# news-react
-这是一个新闻app，用nodejs大后端开发。里面用了express框架搭建服务器。
--- route/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-module.exports = function(db){
-	var router = express.Router();
-	router.get("/",function(req,res){
-		req.session["user_id"] = undefined;
-		res.redirect("/");
-	})
-	return router;
-}
